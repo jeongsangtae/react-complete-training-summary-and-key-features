121eb5799f637d2fd0246ec41ada370600824354
jeongsangtae <wjdtkdxo55@naver.com>
2023-12-02 05:25:28 +0900

    React Complete Training Summary and Key Features - raise status

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
 
 const PostList = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Jeong" body="sang tae" />
-        <Post author="Kim" body="hun sun" />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Kim" body="Hun" />
       </ul>
     </>
   );
