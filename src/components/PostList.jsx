c64bd59318c274dec40de1e91aaf47246d0bf0b8
jeongsangtae <wjdtkdxo55@naver.com>
2023-12-03 22:43:42 +0900

    React Complete Training Summary and Key Features - loading message add

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,12 +7,15 @@ import classes from "./PostList.module.css";
 
 const PostList = ({ isPosting, onStopPosting }) => {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     };
 
     fetchPosts();
@@ -37,17 +40,22 @@ const PostList = ({ isPosting, onStopPosting }) => {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => {
             return <Post key={post.id} author={post.author} body={post.body} />;
           })}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
